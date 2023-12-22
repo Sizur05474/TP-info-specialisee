e66c2b590d7c487135a17511dcaeb209eb16dd5c
Sizur05474 <150800239+Sizur05474@users.noreply.github.com>
2023-12-22 11:05:31 +0100

    Update and rename sketch_nov28a.ino to Usart0Transmit&Receive 1.0.ino

--- Usart0Transmit&Receive 1.0.ino
+++ Usart0Transmit&Receive 1.0.ino
@@ -1,12 +1,36 @@
+/**
+ * @file USART0
+ * @author Lucas Borel
+ */
+/**
+ * @brief Vérification de la bonne définition du BAUD
+ */
 #define FRAMING_ERROR (1<<FE0)
+/**
+ * @brief Définition d'une possible erreur de parité
+ */
 #define PARITY_ERROR (1<<UPE0)
+/**
+ * @brief Erreur de data définit au-delà de l'espace mémoire
+ */
 #define DATA_OVERRUN (1<<DOR0)
+/**
+ * @brief Définition de la fréquence d'oscillation à 16 MHz
+ */
 #define FOSC 16000000 // Clock Speed
+/**
+ * @brief Définition de BAUD à 9600 pour le port série
+ */
 #define BAUD 9600
+/**
+ * @brief Calcul pour le taux du port série
+ */
 #define MYUBRR FOSC/16/BAUD-1
 int flag=0;
 
-
+/**
+ * @brief Initialisation des variables pour pouvoir utiliser transmit and receive et avoir un bit de stop à 2
+ */
 void USART_Init(unsigned int ubrr)
 {
 /*Set baud rate */
@@ -17,7 +41,9 @@ UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
 /* Set frame format: 8data, 2stop bit */
 UCSR0C = (1<<USBS0)|(1<<UCSZ00)|(1<<UCSZ01);
 }
-
+/**
+ * @brief Fonction transmit
+ */
 void USART_Transmit(unsigned char data)
 {
 /* Wait for empty transmit buffer */
@@ -25,7 +51,9 @@ while (!(UCSR0A & (1<<UDRE0)));
 /* Put data into buffer, sends the data */
 UDR0 = data;
 }
-
+/**
+ * @brief Fonction permettant de sauter une ligne
+ */
 void USART_puts(unsigned char *str)
 {
     do
@@ -41,7 +69,9 @@ USART_putsln(unsigned char *str)
      USART_Transmit(10);
      USART_Transmit(13);}
 }*/
-
+/**
+ * @brief Fonction permettant de recevoir les caractères transmits
+ */
 unsigned char USART_Receive(void)
 {
 /* Wait for data to be received */
@@ -50,7 +80,10 @@ while (!(UCSR0A & (1<<RXC0)));
 return UDR0;
 }
 
-
+/**
+ *@brief S'il n'y a aucune erreur on passe le flag à 1 sinon on affiche "error" 
+ * @param flag Variable pour l'interruption
+ */
 ISR(USART_RX_vect) // https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
 {
   char status=UCSR0A;
@@ -61,7 +94,9 @@ if((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
     {UDR0;
      USART_puts("error");}
 }
-
+/**
+ * @brief Utilisation de l'interruption dans le main pour afficher le caractère suivant sinon ne rien afficher
+ */
 int main()
 {
 USART_Init(MYUBRR);
